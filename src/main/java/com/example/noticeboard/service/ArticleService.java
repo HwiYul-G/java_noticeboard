75b55f83d8d463337564c23ab698e9f7c18ed3dd
kim <hwiyul_g@daum.net>
2024-03-08 22:40:51 +0900

    feat(ArticleService) : service는 repository와 비슷 해 보이지만 비지니스 관점에서 보기 좋도록 이름일 짓는 걸 볼 수 있따.

---
+++ src/main/java/com/example/noticeboard/service/ArticleService.java
@@ -0,0 +1,59 @@
+package com.example.noticeboard.service;
+
+import com.example.noticeboard.domain.Article;
+import com.example.noticeboard.repository.ArticleRepository;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+public class ArticleService {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository){
+        this.articleRepository = articleRepository;
+    }
+
+    public Long createArticle(Article article) {
+        validateDuplicateTitle(article);
+        articleRepository.save(article);
+        return article.getId();
+    }
+
+    private void validateDuplicateTitle(Article article){
+        // Optional<>이 보이는 게 좋지 않아서 한줄로 변경
+        articleRepository.findByTitle(article.getTitle())
+                .ifPresent(a -> {
+                    throw new IllegalStateException("이미 존재하는 제목입니다.");
+                });
+    }
+
+    public List<Article> findArticles(){
+        return articleRepository.findAll();
+    }
+
+    public Optional<Article> findOne(Long articleId){
+        return articleRepository.findById(articleId);
+    }
+
+    public Optional<Article> findOne(String title){
+        return articleRepository.findByTitle(title);
+    }
+
+    public List<Article> findArticlesByWriter(String writer){
+        return articleRepository.findByWriter(writer);
+    }
+
+    public List<Article> findArticlesByDate(String date){
+        LocalDate localDateTime = parseStringToLocalDateTime(date);
+        return articleRepository.findByCreatedAt(localDateTime);
+    }
+
+    private LocalDate parseStringToLocalDateTime(String date){
+        return LocalDate.parse(date, formatter);
+    }
+
+
+}
