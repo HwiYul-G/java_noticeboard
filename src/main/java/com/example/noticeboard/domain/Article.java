30f03ab26ef75f661d3f66e8088ea586000494c3
kim <hwiyul_g@daum.net>
2024-03-13 17:24:15 +0900

    feat : jpa를 위해 entity처리

--- src/main/java/com/example/noticeboard/domain/Article.java
+++ src/main/java/com/example/noticeboard/domain/Article.java
@@ -1,8 +1,15 @@
 package com.example.noticeboard.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Article {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
