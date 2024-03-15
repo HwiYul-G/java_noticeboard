f499ef7e25fb70f5951b9b03e07f3a321810fd6b
kim <hwiyul_g@daum.net>
2024-03-15 23:20:51 +0900

    feat(CommentRepository) : findById 추가

--- src/main/java/com/example/noticeboard/repository/CommentRepository.java
+++ src/main/java/com/example/noticeboard/repository/CommentRepository.java
@@ -3,9 +3,11 @@ package com.example.noticeboard.repository;
 import com.example.noticeboard.domain.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository {
     Comment save(Comment comment);
     List<Comment> findByArticleId(Long articleId);
+    Optional<Comment> findById(Long id);
     void deleteById(Long id);
 }
