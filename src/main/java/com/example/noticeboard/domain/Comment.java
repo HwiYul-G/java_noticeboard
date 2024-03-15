6fb5bec2f4ac102b41f912c92ac56ac7038641aa
kim <hwiyul_g@daum.net>
2024-03-15 23:20:12 +0900

    fet(Comment) : CommentDTO로 Comment Entity 생성 + patch(업데이트) 코드 추가

--- src/main/java/com/example/noticeboard/domain/Comment.java
+++ src/main/java/com/example/noticeboard/domain/Comment.java
@@ -1,8 +1,15 @@
 package com.example.noticeboard.domain;
 
+import com.example.noticeboard.controller.dto.CommentDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
 public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long articleId; // 댓글의 부모 게시글
     private String writer;
@@ -11,43 +18,49 @@ public class Comment {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Long getId() {
-        return id;
+
+    public Comment() {
+    }
+
+    public Comment(Long articleId, String writer, String password, String content) {
+        this.articleId = articleId;
+        this.writer = writer;
+        this.password = password;
+        this.content = content;
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getArticleId() {
-        return articleId;
-    }
-
     public void setArticleId(Long articleId) {
         this.articleId = articleId;
-    }
-
-    public String getWriter() {
-        return writer;
     }
 
     public void setWriter(String writer) {
         this.writer = writer;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getContent() {
-        return content;
     }
 
     public void setContent(String content) {
         this.content = content;
     }
+
+    public static Comment createComment(CommentDto dto, Long articleId){
+        return new Comment(articleId, dto.writer(), dto.password(), dto.content());
+    }
+
+    public void patch(CommentDto dto){
+        this.writer = dto.writer();
+        this.password = dto.password();
+        this.content = dto.content();
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
